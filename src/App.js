90453d6611486f120f528316ace610c086f7fc75
shiwam9916 <69190790+shiwam9916@users.noreply.github.com>
2023-01-19 13:17:38 +0530

    par

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Navbar / >
+        <
+        /div>
+    );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar(){
+    return <nav className="nav">
+        <a href="/" className="site-title">Site Name</a>
+        <ul>
+            <li>
+            <a href="/pr">Price</a>
+            </li>
+            <li>
+            <a href="/ab">About</a>
+            </li>
+            <li>
+            <a href="/ab">About</a>
+            </li>
+        </ul>
+    </nav> 
+}
